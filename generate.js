f88e6abd3578e4cd83cc4ad994e6fd14b9caf9d4
Greg Malysa <gmalysa@stanford.edu>
2013-05-12 04:14:49 +0000

    Generates, but doesn't respect all arguments yet.

--- generate.js
+++ generate.js
@@ -10,7 +10,6 @@ var fs = require('fs');
 require('colors');
 
 var fl = require('flux-link');
-var db = require('./db-filters');
 var spy = require('spyglass');
 var gls = new spy();
 
@@ -24,6 +23,14 @@ c.version('0.1.0')
 	.option('-d, --database <name>', 'Database name to generate filters for')
 	.option('-o, --output <./filters>', 'Output path for filter definitions')
 	.parse(process.argv);
+
+function s(str, colors) {
+	return _.reduce(colors, function(memo, v) { return memo[v]; }, s);
+}
+
+function colorize_prompt(msg, colors) {
+	
+}
 
 function do_prompt(field, msg) {
 	return function (env, after) {
@@ -58,7 +65,7 @@ function check_prompt(field, defval) {
 }
 
 function get_field_type(type) {
-	var match = type.match(/([^\(]*)\((\d+)\)/);
+	var match = type.match(/([^\(]*)(?:\((\d+)\))?/);
 	var ftype = match[1];
 	var length = match[2];
 
@@ -66,9 +73,58 @@ function get_field_type(type) {
 		return 'db.int_t';
 	else if (type.match(/varchar/i))
 		return '[db.varchar_t, '+length+']';
+	else if (type.match(/datetime/i))
+		return 'db.datetime_t';
+	else if (type.match(/timestamp/i))
+		return 'db.timestamp_t';
+	else if (type.match(/text/i))
+		return 'db.text_t';
 	else
 		return undefined;
 }
+
+var get_filter_name = new fl.Chain(
+	function (env, after, table) {
+		env.table = table;
+		env.filter = table;
+		c.prompt('Filter name for table '+table+' ['+table+']: ', after);
+	},
+	function (env, after, value) {
+		if (value.length > 0)
+			env.filter = value;
+		after();
+	}
+);
+
+var process_table = new fl.Chain(
+	get_filter_name,
+	function (env, after) {
+		env.conn.query('DESCRIBE '+c.database+'.'+env.table, after);
+	},
+	function (env, after, err, rows) {
+		var cols = [];
+		_.each(rows, function(v) {
+			var type = get_field_type(v.Type);
+			if (type === undefined)
+				console.log('WARNING:'.bold.red+' Field '+v.Field+' of type '+v.Type+' does not match any types that db-filters currently understands. It will be omitted');
+			else
+				cols.push(v.Field+' : '+get_field_type(v.Type));
+		});
+		var output = 'module.exports = function(db) {\n';
+		output += '\tvar cols = {\n\t\t';
+		output += cols.join(',\n\t\t');
+		output += '\n\t};\n\n\tdb.add_filter("'+env.filter+'", new db("'+env.table+'", cols, {});\n}\n';
+		fs.writeFile(c.output+'/'+env.filter+'.js', output, after);
+	},
+	function (env, after, err) {
+		if (err) {
+			env.$throw(err);
+		}
+		else {
+			after();
+		}
+	}
+);
 
 var main = new fl.Chain(
 	do_prompt('host', 'MySQL host [localhost]: '),
@@ -110,16 +166,17 @@ var main = new fl.Chain(
 		}
 		else {
 			rows = _.map(rows, function(v) { return _.values(v)[0]; });
-			env.conn.query('DESCRIBE '+c.database+'.'+rows[0], after);
+			after(rows, after);
 		}
 	},
-	function (env, after, err, rows) {
-		if (err)
-			env.$throw(err);
-		_.each(rows, function(v) {
-			logger.var_dump(get_field_type(v.Type), v.Field);
-		});
-		after();
+	function (env, after, rows, loop) {
+		if (rows.length > 0) {
+			var row = rows.shift();
+			process_table.call(null, env, _.partial(loop, rows, loop), row);
+		}
+		else {
+			after();
+		}
 	}
 );
 
